c436b94200acdaf755e8a741b528df5ee53928e8
Selena Yang <syang@lcs.on.ca>
2019-10-03 21:11:33 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Yang, Selena on 2019/10/3.
+//  Copyright © 2019 Yang Selena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
